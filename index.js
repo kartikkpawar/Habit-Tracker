f4b5ba59bba74b7a89f20370b4a31cabd124b0d5
Kartik Pawar <knpawar789@gmail.com>
2022-06-06 17:48:41 +0530

    Heroku Config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("./config/db");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // EJS Config
 //EJS Config
