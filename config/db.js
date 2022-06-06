900b660bf25b49aad8ad197122ea4a5fb9492dc4
Kartik Pawar <knpawar789@gmail.com>
2022-06-06 17:55:18 +0530

    Habit Tracker

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 mongoose
-  // .connect(process.env.MONGO_DB)
-  .connect("mongodb://localhost:27017/habitTracker")
+  .connect(process.env.MONGO_DB)
+  // .connect("mongodb://localhost:27017/habitTracker")
   .then((res) => console.log("DB Connected"))
   .catch((err) => console.log("Unable to connect to DB"));
